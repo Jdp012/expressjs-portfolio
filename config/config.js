2d80c641ef8cc7335a192732b5885fb6986ecac9
jdp012 <jayqatariya@gmail.com>
2022-10-08 13:00:57 -0400

    first commit

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+//export const MongoURI  = "mongodb://127.0.0.1/media";
+export const MongoURI = "mongodb+srv://JDXII:QAwsED8055@cluster0.bhga4ah.mongodb.net/media?retryWrites=true&w=majority";
